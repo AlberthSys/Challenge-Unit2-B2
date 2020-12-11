e92a6e5feca6f27c020ec504e0f385a00cc33e16
Alberth <bcajavier@gmail.com>
2020-12-11 22:22:46 +0100

    Comments

--- StarWars.java
+++ StarWars.java
@@ -1,18 +1,19 @@
 import java.util.*;
-
-public class Reto1 {
+//ALBERTO J BAENA
+//we seek relationship and hide the key of revelation
+public class StarWars {
 
     public static void main (String[] args){
 
         Scanner sc = new Scanner(System.in);
-
+        //numbers of try
         int numberTry = sc.nextInt();
 
         for (int i = 0; i<numberTry;i++){
 
             String names = sc.next();
             String relationship = sc.next();
-
+            //Looking the same
             if(names.equals("Luke") && relationship.equals("padre")){
                 System.out.println("TOP SECRET");
             }
