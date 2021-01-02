12afba8ac404dfd36310fba9c210f728a2764519
Alberth <73870637+AlberthSys@users.noreply.github.com>
2021-01-02 10:19:16 +0100

    Update CircularPrime.java

--- CircularPrime.java
+++ CircularPrime.java
@@ -1,11 +1,4 @@
 
-/*Create a function that returns if a number is Circular prime or not. A
-Circular prime number is the number that is prime in every rotation of their
-digits. For example: 1193 is circular prime because 1931,9311,3119 and 1193 are
-prime numbers. https://en.wikipedia.org/wiki/Circular_prime Hint: You can use
-the number as a String to make the rotations and transform it later to integer
-to make the divisions. Remember to catch all the possible exceptions.*/
-//ALBERTO JAVIER BAENA
 import java.util.Scanner;
 public class CircularPrime {
     public static void main(String[] args) {
